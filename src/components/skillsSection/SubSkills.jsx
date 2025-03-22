c85bacd5d7621e091ca1ebe6b8fc4727d7c71626
invicta7 <vick4dermatology@gmail.com>
2025-03-22 23:05:39 +0100

    eigth commit

--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -4,7 +4,7 @@ const SubSkills = () => {
   return (
     <div className="border-y-2 border-lightGrey relative">
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
-      <img src="/public/assets/images/subSkills.jpg" alt="Subskill image" />
+      <img src="src/assets/images/subSkills.jpg" alt="Subskill image" />
     </div>
   );
 };
