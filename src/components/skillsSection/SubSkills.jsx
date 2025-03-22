0d304f0752daba12558e035d8ad16f567d3966dd
invicta7 <vick4dermatology@gmail.com>
2025-03-23 00:15:58 +0100

    18th commit

--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import subImage from "..assets/Subskills.jpeg";
 
 const SubSkills = () => {
   return (
     <div className="border-y-2 border-lightGrey relative">
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
-      <img src="src/assets/SubSkills.jpeg" alt="Subskill image" />
+      <img src={subImage} alt="Subskill image" />
     </div>
   );
 };
